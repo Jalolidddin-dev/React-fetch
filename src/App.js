deba2e57c2c897aee820219fc1eaf4d849e1cc75
Dew0277 <jaloliddina98@gmail.com>
2024-10-05 05:13:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Loading from "./components/Loading";
+
+let API = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUserHadler = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(API);
+      const data = await response.json();
+
+      setUsers(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserHadler();
+  }, []);
+
+  //   const [count, setCount] = useState(0);
+
+  //   const increment = () => {
+  //     setCount(count + 1);
+  //   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="users_box">
+      {!isLoading ? (
+        <div>
+          {users && users.length > 0 && !isError ? (
+            users.map((user) => (
+              <div key={user.id} className="user_box">
+                <div>
+                  <h1>User Name :</h1>
+                  <span>{user.name}</span>
+                </div>
+                <div>
+                  <h2>User Email:</h2>
+                  <span>{user.email}</span>
+                </div>
+                <div>
+                  <h2>User address:</h2>
+                  <span>{user.address.city}</span>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>
+              <h1>User not Found</h1>
+            </div>
+          )}
+        </div>
+      ) : (
+        <Loading />
+      )}
+
+      {/* <div>
+        <h1>{count}</h1>
+        <button onClick={increment}>+</button>
+      </div> */}
     </div>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import "./Loading.css";
+function Loading() {
+  return (
+    <div className="loading_box">
+      <AiOutlineLoading3Quarters className="loading_icon" />
+    </div>
+  );
+}
+
+export default Loading;
